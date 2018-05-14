36da10de37f0e8d2b4bea9455ba91177af0f24ad
Jan Musílek <jan.musilek@nic.cz>
2018-05-14 14:08:44 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! Add basic bank statement parsers

--- django_pain/parsers/common.py
+++ django_pain/parsers/common.py
@@ -10,10 +10,10 @@ class AbstractBankStatementParser(ABC):
         """
         Parse bank statement.
 
-        Each parser class has to implement this method. Result is either iterator
-        returning BankPayment objects or iterator returning sequence.
+        Each parser class has to implement this method. Result is either iterable
+        of BankPayment objects or iterable of sequences.
 
-        If result is iterator of sequences, first element of each sequence has to
+        If result is iterable of sequences, first element of each sequence has to
         be BankPayment object. Other elements (if any) should be other payment
         related objects such as PaymentSymbols.
 
