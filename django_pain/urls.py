0f23e286f92160bc9cbcacbf7eae3ec0c6551aad
Jan Musílek <jan.musilek@nic.cz>
2018-05-17 15:28:59 +0200

    fixup! fixup! Add PaymentsListView and template

--- django_pain/urls.py
+++ django_pain/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 from .views import PaymentListView
 
 urlpatterns = [
-    path('payments/', PaymentListView.as_view()),
+    path('payments/', PaymentListView.as_view(), name='payments-list'),
 ]
